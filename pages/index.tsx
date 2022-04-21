e8147e19a98523ca998dbb06da700d171064fd93
Solomon S <schinoyerem007@gmail.com>
2022-04-21 03:09:23 +0100

    egetStaticProps Next implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import { services } from '../data';
 
 const Home = () => {
+  console.log({ services });
+
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
