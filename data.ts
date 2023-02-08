04d404034d82f74633f150ad81977e6347b5fd58
iykeprince48@gmail.com <schinoyerem007@gmail.com>
2023-02-08 01:55:34 +0100

    updated resume

--- data.ts
+++ data.ts
@@ -8,207 +8,233 @@ import { BsCircleFill } from "react-icons/bs";
 import { IProject, IService, ISkill } from "./type";
 
 export const services: IService[] = [
-    {
-        Icon: RiComputerLine,
-        title: "Frontend Development",
-        about:
-            "I can build a beautiful and scalable SPA using <b> HTML</b>,<b>CSS</b>   and <b>React.js</b> ",
-    },
-    {
-        Icon: FaServer,
-        title: "Excellent Support for Backend Development",
-        about:
-            "Handle database, server, api using <b>Nest Framework </b> & other popular frameworks",
-    },
-    {
-        Icon: AiOutlineApi,
-        title: "API Development",
-        about:
-            "I can develop robust  REST API using <b>Asp.Net Core</b>  & <b>C#</b> ",
-    },
-    {
-        Icon: RiComputerLine,
-        title: "Skiiled Debugger",
-        about: "A daily problem solver using <b>Browser</b>  and <b>Developer Tool</b> ",
-    },
-    {
-        Icon: AiOutlineAntDesign,
-        title: "UI/UX designer",
-        about:
-            "Stunning user interface designer using <b>Figma</b>",
-    },
-    {
-        Icon: MdDeveloperMode,
-        title: "Mobile Developer",
-        about:
-            "A junior mobile developer using <b>React Native </b>",
-    },
+  {
+    Icon: RiComputerLine,
+    title: "Frontend Development",
+    about:
+      "I can build a beautiful and scalable SPA using <b> HTML</b>,<b>CSS</b>   and <b>React.js</b> ",
+  },
+  {
+    Icon: FaServer,
+    title: "Excellent Support for Backend Development",
+    about:
+      "Handle database, server, api using <b>Nest Framework </b> & other popular frameworks",
+  },
+  {
+    Icon: AiOutlineApi,
+    title: "API Development",
+    about:
+      "I can develop robust  REST API using <b>Asp.Net Core</b>  & <b>C#</b> ",
+  },
+  {
+    Icon: RiComputerLine,
+    title: "Skiiled Debugger",
+    about:
+      "A daily problem solver using <b>Browser</b>  and <b>Developer Tool</b> ",
+  },
+  {
+    Icon: AiOutlineAntDesign,
+    title: "UI/UX designer",
+    about: "Stunning user interface designer using <b>Figma</b>",
+  },
+  {
+    Icon: MdDeveloperMode,
+    title: "Mobile Developer",
+    about: "A junior mobile developer using <b>React Native </b>",
+  },
 ];
 
 export const languages: ISkill[] = [
-    {
-        Icon: BsCircleFill,
-        name: "React",
-        level: "78%",
-    },
-    {
-        Icon: BsCircleFill,
-        name: "Java Script",
-        level: "70%",
-    },
-    {
-        Icon: BsCircleFill,
-        name: "Bootstrap",
-        level: "80%",
-    },
-    {
-        Icon: BsCircleFill,
-        name: "CSS",
-        level: "70%",
-    },
-    {
-        Icon: BsCircleFill,
-        name: "Tailwind",
-        level: "40%",
-    },
-    {
-        Icon: BsCircleFill,
-        name: "C#",
-        level: "45%",
-    },
+  {
+    Icon: BsCircleFill,
+    name: "React",
+    level: "78%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Java Script",
+    level: "70%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Bootstrap",
+    level: "80%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "CSS",
+    level: "70%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Tailwind",
+    level: "40%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "C#",
+    level: "45%",
+  },
 
-    {
-        Icon: BsCircleFill,
-        name: "React Native",
-        level: "50%",
-    },
+  {
+    Icon: BsCircleFill,
+    name: "React Native",
+    level: "50%",
+  },
 
+  {
+    Icon: BsCircleFill,
+    name: "Solidity",
+    level: "30%",
+  },
 
-    {
-        Icon: BsCircleFill,
-        name: "Solidity",
-        level: "30%",
-    },
+  {
+    Icon: BsCircleFill,
+    name: "Redux",
+    level: "80%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Nextjs",
+    level: "70%",
+  },
 ];
 
 export const tools: ISkill[] = [
+  {
+    Icon: BsCircleFill,
+    name: "Version Control",
+    level: "80%",
+  },
 
-    {
-        Icon: BsCircleFill,
-        name: "Version Control",
-        level: "80%",
-    },
-
-    {
-        Icon: BsCircleFill,
-        name: "Microdoft Office",
-        level: "70%",
-    },
-    {
-        Icon: BsCircleFill,
-        name: "Figma",
-        level: "50%",
-    },
-
+  {
+    Icon: BsCircleFill,
+    name: "Microsoft Office",
+    level: "70%",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Figma",
+    level: "50%",
+  },
 ];
 
 export const projects: IProject[] = [
-    {
-        id: 1,
-        name: "Handjobs",
-        description:
-            "Handjobs - A market place for skilled workers (Artisans)to be instantly reached by everyone who needs their service",
-        image_path: "/images/handjobs.png",
-        deployed_url: "https://jobplicant-artisan-kohl.vercel.app/",
-        github_url: "https://github.com/kodlix/jobplicant-artisan",
-        category: ["react", "bootstrap", "node", "redux", "rest_api"],
-        key_techs: ["React", "Nodejs", "PrimeReact", "Bootstrap", "CSS"],
-    },
-    {
-        id: 2,
-        name: "Kapsuul",
-        image_path: "/images/kapsuul.png",
-        deployed_url: "https://www.kapsuulconnect.com/",
-        github_url: "",
-        category: ["react"],
-        description:
-            "A web app that helps Connect you to great minds and giving you insights to build, and grow your practice.",
-        key_techs: ["React", "Nodejs", "Postgres", "Bootstrap", "Typescript"],
-    },
+  {
+    id: 1,
+    name: "Handjobs",
+    description:
+      "Handjobs - A market place for skilled workers (Artisans)to be instantly reached by everyone who needs their service",
+    image_path: "/images/handjobs.png",
+    deployed_url: "https://handjobs-web.vercel.app/",
+    github_url: "https://github.com/solomonsunday/handjobs-web",
+    category: ["react", "bootstrap", "node", "redux", "rest_api"],
+    key_techs: ["React", "Nodejs", "PrimeReact", "Bootstrap", "CSS"],
+  },
+  {
+    id: 2,
+    name: "Kapsuul",
+    image_path: "/images/kapsuul.png",
+    deployed_url: "https://www.kapsuulconnect.com/",
+    github_url: "",
+    category: ["react", "redux"],
+    description:
+      "A web app that helps Connect you to great minds and giving you insights to build, and grow your practice.",
+    key_techs: ["React", "Nodejs", "Postgres", "Bootstrap", "Typescript"],
+  },
 
-    {
-        id: 3,
-        name: "Woozeee",
-        image_path: "/images/woozeee.png",
-        deployed_url: "https://woozeee.com/",
-        github_url: "",
-        category: ["node", "postgres", "react", "Material UI", "tailwind"],
-        description:
-            "Socials, Movies, Get Paid, Bills Payment, Loans, All in One App...",
-        key_techs: [
-            "React",
-            "Redux",
-            "Node",
-            "Express",
-            "Mongo",
-            "REST API",
-            "Bootstrap",
-        ],
-    },
+  {
+    id: 3,
+    name: "Woozeee",
+    image_path: "/images/woozeee.png",
+    deployed_url: "https://woozeee.com/",
+    github_url: "",
+    category: ["node", "postgres", "react", "Material UI", "redux", "tailwind"],
+    description:
+      "Socials, Movies, Get Paid, Bills Payment, Loans, All in One App...",
+    key_techs: [
+      "React",
+      "Redux",
+      "Node",
+      "Express",
+      "Mongo",
+      "REST API",
+      "Bootstrap",
+    ],
+  },
 
-    {
-        id: 4,
-        name: "Krypto",
-        image_path: "/images/krypto.png",
-        deployed_url: "",
-        github_url: "https://github.com/solomonsunday/krypto",
-        category: ["react", "solidity", "tailwind"],
-        description:
-            "A Smart Contract used to transfer Etherium from one Wallet to another, built using React, Tailwind, and Solidity 0.8.0",
-        key_techs: ["React", "solidity", "Tailwind"],
-    },
-    {
-        id: 5,
-        name: "Discover Coffee Store Near You",
-        image_path: "/images/coffee_store_app.png",
-        deployed_url: "",
-        github_url: "https://github.com/solomonsunday/Discover-Coffee-store",
-        category: ["react", "nextjs", "bootstrap",],
-        description: "An application thats helps user to discover coffee stores near and around them",
-        key_techs: ["React", "Bootstrap", "Nextjs"]
+  {
+    id: 4,
+    name: "Krypto",
+    image_path: "/images/krypto.png",
+    deployed_url: "",
+    github_url: "https://github.com/solomonsunday/krypto",
+    category: ["react", "solidity", "tailwind"],
+    description:
+      "A Smart Contract used to transfer Etherium from one Wallet to another, built using React, Tailwind, and Solidity 0.8.0",
+    key_techs: ["React", "solidity", "Tailwind"],
+  },
+  {
+    id: 5,
+    name: "Discover Coffee Store Near You",
+    image_path: "/images/coffee_store_app.png",
+    deployed_url: "",
+    github_url: "https://github.com/solomonsunday/Discover-Coffee-store",
+    category: ["react", "nextjs", "bootstrap"],
+    description:
+      "An application thats helps user to discover coffee stores near and around them",
+    key_techs: ["React", "Bootstrap", "Nextjs"],
+  },
 
-    }
+  {
+    id: 6,
+    name: "Persist Africa",
+    image_path: "/images/persistAfrica.png",
+    deployed_url: "https://dev.app.persistafrica.com/",
+    github_url:
+      "https://github.com/ExquisAppFactory/talentbuildersapp/tree/dev",
+    category: ["react", "nextjs", "bootstrap", "redux"],
+    description: "An application that helps africa learn any native skill",
+    key_techs: [
+      "React",
+      "Bootstrap",
+      "Nextjs",
+      "redux",
+      "Styled Component",
+      "AWS",
+    ],
+  },
 
-    // {
-    //     name: "Tweeter Clone",
-    //     image_path: "/images/kapsuul.png",
-    //     deployed_url: "http://sumaxtweetme.pythonanywhere.com/",
-    //     github_url: "",
-    //     category: ["react", "react"],
-    //     description:
-    //         "First Django Project :) | Typical Social Media App where one can post,like ,comment etc",
-    //     key_techs: ["React", "Django", "Django REST API"],
-    // },
+  // {
+  //     name: "Tweeter Clone",
+  //     image_path: "/images/kapsuul.png",
+  //     deployed_url: "http://sumaxtweetme.pythonanywhere.com/",
+  //     github_url: "",
+  //     category: ["react", "react"],
+  //     description:
+  //         "First Django Project :) | Typical Social Media App where one can post,like ,comment etc",
+  //     key_techs: ["React", "Django", "Django REST API"],
+  // },
 
-    // {
-    //     name: "Color Classification using tf.js",
-    //     image_path: "/images/kapsuul.png",
-    //     deployed_url: "!#",
-    //     github_url: "",
-    //     category: ["node"],
-    //     description:
-    //         "Tried ML with JS :) | this app classifies a color using CNN algorithm in browser",
-    //     key_techs: ["Express", "TensorFlow.js", "Vanilla js"],
-    // },
+  // {
+  //     name: "Color Classification using tf.js",
+  //     image_path: "/images/kapsuul.png",
+  //     deployed_url: "!#",
+  //     github_url: "",
+  //     category: ["node"],
+  //     description:
+  //         "Tried ML with JS :) | this app classifies a color using CNN algorithm in browser",
+  //     key_techs: ["Express", "TensorFlow.js", "Vanilla js"],
+  // },
 
-    // {
-    //     name: "YouTube Clone",
-    //     image_path: "/images/kapsuul.png",
-    //     deployed_url: "https://o-my-goal.web.app/",
-    //     github_url: "",
-    //     category: ["react"],
-    //     description:
-    //         "o my goal replica where an user can keep an eye on his favorite club.This app will keep providing \n all the statistics of that club.all the fans can also chat ",
-    //     key_techs: ["React", "Redux", "Firebase Auth", "API", "Sass", "Bootstrap"],
-    // },
+  // {
+  //     name: "YouTube Clone",
+  //     image_path: "/images/kapsuul.png",
+  //     deployed_url: "https://o-my-goal.web.app/",
+  //     github_url: "",
+  //     category: ["react"],
+  //     description:
+  //         "o my goal replica where an user can keep an eye on his favorite club.This app will keep providing \n all the statistics of that club.all the fans can also chat ",
+  //     key_techs: ["React", "Redux", "Firebase Auth", "API", "Sass", "Bootstrap"],
+  // },
 ];
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,99 +1,135 @@
-import { motion } from 'framer-motion'
-import React from 'react'
-import { BsCircleFill } from 'react-icons/bs'
-import { fadingUP, routeAnimation } from '../animations'
-import Bar from '../components/Bar'
-import { languages, tools } from '../data'
+import { motion } from "framer-motion";
+import React from "react";
+import { BsCircleFill } from "react-icons/bs";
+import { fadingUP, routeAnimation } from "../animations";
+import Bar from "../components/Bar";
+import { languages, tools } from "../data";
 
 const Resume = () => {
-
-    return (
-        <motion.div variants={routeAnimation}
-            initial="initial" animate="animate" className='px-6 py-2'>
-            {/* //education */}
-            <motion.div variants={fadingUP} initial="initial" animate="animate" className="grid gap-6 md:grid-cols-2">
-                <div>
-                    <h5 className='my-2 text-2xl font-bold'>Education</h5>
-                    <div className='flex items-center'>
-                        {/* <div className='mx-2'>
+  return (
+    <motion.div
+      variants={routeAnimation}
+      initial="initial"
+      animate="animate"
+      className="px-6 py-2"
+    >
+      {/* //education */}
+      <motion.div
+        variants={fadingUP}
+        initial="initial"
+        animate="animate"
+        className="grid gap-6 md:grid-cols-2"
+      >
+        <div>
+          <h5 className="my-2 text-2xl font-bold">Education</h5>
+          <div className="flex items-center">
+            {/* <div className='mx-2'>
                             <BsCircleFill fontSize={10} />
                         </div> */}
-                        <div className='mb-8'>
-                            <h2 className='my-2 text-xl font-bold'>Nnamdi Azikiwe University </h2>
-                            <p className='font-semibold'>Computer Science(2013-2017)</p>
-                        </div>
-                    </div>
-                    <div className='flex items-center'>
-                        {/* <div className='mx-2'>
+            <div className="mb-5">
+              <h2 className="my-2 text-xl font-bold">
+                Nnamdi Azikiwe University{" "}
+              </h2>
+              <p className="font-semibold">Computer Science ( 2013-2017 )</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            {/* <div className='mx-2'>
                             <BsCircleFill fontSize={10} />
                         </div> */}
-                        <div>
-                            <h5 className='my-2 text-xl font-bold'>Udacity </h5>
-                            <p className='font-semibold'>FrontEnd Developer(2018-2018)</p>
-                        </div>
+            <div className="mb-5">
+              <h5 className="my-2 text-xl font-bold">Udacity </h5>
+              <p className="font-semibold">FrontEnd Developer( 2018 )</p>
+              <p className="font-semibold">
+                Nanodegree, Cloud DevOps Engineer( 2022 )
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div>
+              <h5 className="my-2 text-xl font-bold">
+                Zero To Mastery Academy{" "}
+              </h5>
+              <p className="font-semibold">Next Js( 2022 )</p>
+              <p className="font-semibold">
+                Complete Next.js Developer ( 2022 )
+              </p>
+            </div>
+          </div>
+        </div>
 
-                    </div>
-                </div>
+        <motion.div variants={fadingUP} initial="initial" animate="animate">
+          <h5 className="my-2 text-2xl font-bold">Experience</h5>
+          <div>
+            <div className="mb-6">
+              <h5 className="my-2 text-xl font-bold">
+                FrontEnd Developer Engineer{" "}
+                <span className="text-sm">(Lead)</span>
+                <span className="text-sm"></span>
+              </h5>
+              <p className="font-semibold">
+                Exquis App Factory LTD (2022 - on)
+              </p>
+            </div>
+            <div className="mb-6">
+              <h5 className="my-2 text-xl font-bold">
+                FrontEnd Developer Engineer <span className="text-sm"></span>
+              </h5>
+              <p className="font-semibold">Datamax Files LTD (2021 - 2022)</p>
+            </div>
 
-                <motion.div variants={fadingUP} initial="initial" animate="animate" >
-                    <h5 className='my-2 text-2xl font-bold'>Experience</h5>
-                    <div>
-                        <div className='mb-6'>
-                            <h5 className='my-2 text-xl font-bold'>FrontEnd Developer Engineer <span className='text-sm'></span></h5>
-                            <p className='font-semibold'>Datamax Files LTD (2021 - on)</p>
-                        </div>
+            <div className="mb-6">
+              <h5 className="my-2 text-xl font-bold">
+                Software Engineer{" "}
+                <span className="text-sm">(Intermidiate)</span>
+              </h5>
+              <p className="font-semibold">
+                Netop Business System LTD(2020 - 2021)
+              </p>
+            </div>
 
-                        <div className='mb-6'>
-                            <h5 className='my-2 text-xl font-bold'>Software Engineer <span className='text-sm'>(Intermidiate)</span></h5>
-                            <p className='font-semibold'>Netop Business System LTD(2020 - 2021)</p>
-                        </div>
+            <div className="mb-6">
+              <h5 className="my-2 text-xl font-bold">
+                Software Engineer <span className="text-sm">(Jr)</span>
+              </h5>
+              <p className="font-semibold">
+                Primatus Technology LTD(2019 - 2020)
+              </p>
+            </div>
 
-                        <div className='mb-6'>
-                            <h5 className='my-2 text-xl font-bold'>Software Engineer <span className='text-sm'>(Jr)</span></h5>
-                            <p className='font-semibold'>Primatus Technology LTD(2019 - 2020)</p>
-                        </div>
-
-                        {/* <h5 className='my-2 text-xl font-bold'>Team</h5>
+            {/* <h5 className='my-2 text-xl font-bold'>Team</h5>
                         <p className='font-semibold'>TCS (2021 - on)</p>
                         <h5 className='my-2 text-xl font-bold'>Software Engineer Jr</h5>
                         <p className='font-semibold'>TCS (2021 - on)</p> */}
-
-                    </div>
-                </motion.div>
-
-            </motion.div>
-
-            {/* languages and tools  */}
-
-            <div className="grid gap-6 md:grid-cols-2">
-                <div>
-                    <h5 className='my-3 text-2xl font-bold'>
-                        Languages & Frameworks
-                    </h5>
-
-                    <div className="my-2">
-                        {
-                            languages.map(Language => <Bar data={Language} key={Language.name} />)
-                        }
-                    </div>
-                </div>
-
-                <div>
-                    <h5 className='my-3 text-2xl font-bold'>
-                        Tools & Softwares
-                    </h5>
-
-                    <div className="my-2">
-                        {
-                            tools.map(tool => <Bar data={tool} key={tool.name} />)
-                        }
-                    </div>
-                </div>
-
-
-            </div>
+          </div>
         </motion.div>
-    )
-}
+      </motion.div>
 
-export default Resume
+      {/* languages and tools  */}
+
+      <div className="grid gap-6 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Languages & Frameworks</h5>
+
+          <div className="my-2">
+            {languages.map((Language) => (
+              <Bar data={Language} key={Language.name} />
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
+
+          <div className="my-2">
+            {tools.map((tool) => (
+              <Bar data={tool} key={tool.name} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Resume;
