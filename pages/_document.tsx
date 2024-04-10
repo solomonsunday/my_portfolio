ea1cc09e953d834b4733a4b30d751ed63f420f73
solomon sunday <schinoyerem007@gmail.com>
2024-04-10 16:51:57 +0100

    implemented microsoft clarity for analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,50 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    // static async getInitialProps(ctx) {
-    //     const initialProps = await Document.getInitialProps(ctx)
-    //     return { ...initialProps }
-    // }
+  // static async getInitialProps(ctx) {
+  //     const initialProps = await Document.getInitialProps(ctx)
+  //     return { ...initialProps }
+  // }
 
-    render() {
-        return (
-            <Html lang='en' >
-                <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
-                </Head>
-                <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
+          <Main />
+          <NextScript />
+          {/* <Script type="text/javascript">
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "ltlrvqn5g9");
+        </Script> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a] = c[a] || function () { (c[a].q = c[a].q || 
+                    []).push(arguments) };
+                    t=l.createElement(r);
+                    t.async=1;
+                    t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];
+                    y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "ltlrvqn5g9");`,
+            }}
+          />
+          ;
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
